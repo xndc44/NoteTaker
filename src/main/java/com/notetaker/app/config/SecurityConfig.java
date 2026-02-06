f8a6a75676cbd65ca1f83ab61ff402f9afd8210e
xndc44 <60451187+xndc44@users.noreply.github.com>
2026-02-05 19:58:53 -0600

    added some style

--- src/main/java/com/notetaker/app/config/SecurityConfig.java
+++ src/main/java/com/notetaker/app/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/home", "/register", "/login", "/css/**").permitAll()
+                        .requestMatchers("/", "/home", "/register", "/login", "/css/**", "/styles.css").permitAll()
                         .requestMatchers("/notepad/**").authenticated()
                 )
                 .formLogin(httpForm ->
